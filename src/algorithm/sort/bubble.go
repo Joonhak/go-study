026a9443eee149016601ad509bd47ddeb9dbaee0
Joonhak <ofnullable@gmail.com>
2019-06-11 18:59:28 +0900

    Add comment

--- src/algorithm/sort/bubble.go
+++ src/algorithm/sort/bubble.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+/**
+ * Bubble sort
+ * O(n^2) 의 시간복잡도
+ * 배열의 i, i + 1 번째(다음 인덱스) 원소를 비교하여
+ * 큰 원소를 뒤로, 작은 원소를 앞으로 보내는 작업을 반복한다.
+ * 느리지만 간단한 코드로 작성이 가능하다.
+ */
 func main() {
 	arr := [10]int{9, 1, 2, 8, 6, 7, 5, 3, 0, 4}
 	fmt.Println("initial array: ", arr)
--- src/algorithm/sort/selection.go
+++ src/algorithm/sort/selection.go
@@ -2,24 +2,29 @@ package main
 
 import "fmt"
 
+/**
+ * Selection sort
+ * O(n^2) 의 시간 복잡도
+ * 배열을 순회하며 가장 작은 원소를 찾아 앞으로 보내는 작업을 반복한다.
+ */
 func main() {
 	arr := [10]int{9, 1, 2, 8, 6, 7, 5, 3, 0, 4}
 	fmt.Println("initial array: ", arr)
 
-	var min int
+	var minIndex int
 	var tmp int
 
 	for i := 0; i < len(arr); i++ {
-		min = i
+		minIndex = i
 		for j := i + 1; j < len(arr); j++ {
-			if arr[j] < arr[min] {
-				min = j
+			if arr[minIndex] > arr[j] {
+				minIndex = j
 			}
 		}
 
 		tmp = arr[i]
-		arr[i] = arr[min]
-		arr[min] = tmp
+		arr[i] = arr[minIndex]
+		arr[minIndex] = tmp
 	}
 
 	fmt.Println("result: ", arr)
